93205ca69373fe6be43a9d6a69e069493855394d
Roman Kozodoi <romych.sync@gmail.com>
2026-04-29 13:06:40 +0200

    test: #70 tests updated, main use case is kebab-case, camelCase is also added
    Co-authored-by: Erik May <erikmay@users.noreply.github.com>

--- e2e/insideJsConfig/tailwind.config.js
+++ e2e/insideJsConfig/tailwind.config.js
@@ -1,12 +1,12 @@
-import { scopedPreflightStyles } from "../../dist/index.js";
+import { scopedPreflightStyles } from '../../dist/index.js';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   plugins: [
     scopedPreflightStyles({
-      isolationStrategy: "inside",
-      selector: ".twp",
-      except: ".no-twp",
+      isolationStrategy: 'inside',
+      selector: '.twp',
+      except: '.no-twp',
     }),
   ],
 };
--- e2e/outsideJsConfig/tailwind.config.js
+++ e2e/outsideJsConfig/tailwind.config.js
@@ -1,12 +1,12 @@
-import { scopedPreflightStyles } from "../../dist/index.js";
+import { scopedPreflightStyles } from '../../dist/index.js';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   plugins: [
     scopedPreflightStyles({
-      isolationStrategy: "outside",
-      selector: ".no-twp",
-      plus: ".twp",
+      isolationStrategy: 'outside',
+      selector: '.no-twp',
+      plus: '.twp',
     }),
   ],
 };
